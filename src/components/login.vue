af1c8a61f208457ba958041763c064495aac3982
Fzy <84563123@qq.com>
2022-08-06 19:45:40 +0800

    init

---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <el-form :label-position="left" label-width="80px" :model="userInfo">
+            <h2>用户登陆</h2>
+            <el-form-item label="账号">
+                <el-input v-model="userInfo.userName"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="userInfo.uPwd"></el-input>
+            </el-form-item>
+            <el-button type="primary" disabled>主要按钮</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            labelPosition: 'right',
+            formLabelAlign: {
+                userName: '',
+                uPwd: '',
+            }
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
